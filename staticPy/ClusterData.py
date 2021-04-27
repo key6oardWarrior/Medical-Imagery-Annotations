069976f801a39ad87ef185280b841fd4227641af
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-04-26 23:07:16 -0400

    Create ClusterData.py

---
+++ staticPy/ClusterData.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import numpy as np
+
+class ClusterData:
+	'''
+	# Each concept ID is used a key in __cluster
+	# Values will be each cropped image location
+	'''
+	__cluster = {}
+
+	def __init__(self):
+		pass
+
+	def put(self, key):
+		'''
+		# Put key in the dict if the key does not already exists
+		# this will ensure that a key's value is never overridden
+
+		@param <class 'str'> the key to be put in dict
+		'''
+		if (key in self.__cluster.keys()) == False:
+			self.__cluster[key] = []
+
+	def appendValue(self, key, value):
+		'''
+		# Append value to __cluster[key]
+
+		@param <class 'str'> the key to an array that value will be appended to\n
+		@param <class 'str'> value that will get appened to __cluster[key]
+		'''
+		if key in self.__cluster.keys():
+			self.__cluster[key].append(value)
+
+	def makeCSV(self, RESOURCES):
+		'''
+		# Save the cluster data in a file
+
+		@param <class '__main__.Singleton'>
+		'''
+		keys = list(self.__cluster.keys())
+		longestArr = 0
+
+		for i in range(len(keys)):
+			if((self.__cluster[keys[i]] == []) or (self.__cluster[keys[i]] == None)):
+				del self.__cluster[keys[i]] # remove keys with empty values
+			else: # determin longestArr
+				if len(self.__cluster[keys[i]]) > longestArr:
+					longestArr = len(self.__cluster[keys[i]])
+
+		for i in self.__cluster.keys(): # make all values in dict same length
+			appendAmount = longestArr - len(self.__cluster[i])
+			if appendAmount > 0:
+				self.__cluster[i].extend(["N/A"]*appendAmount)
+
+		pd.DataFrame(self.__cluster).to_csv(f"{RESOURCES.PATH}\\filtered{RESOURCES.folderCnt}\\clusteredData.csv", sep=",")
