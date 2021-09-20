347c39f1cb017f71195bb5cca63d860e95a3ce69
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-09-20 12:27:48 -0400

    Added a help message.  Pick which indexes to save differently

--- staticPy/createBatches.py
+++ staticPy/createBatches.py
@@ -1,11 +1,4 @@
-from os import read
-from os.path import exists
-from pandas import read_csv
-from pandas import DataFrame
-from numpy import array
 from sys import argv
-from sys import platform
-from os.path import dirname
 
 SIZE = len(argv)
 if SIZE < 2:
@@ -14,43 +7,50 @@ if SIZE < 2:
 del SIZE
 
 slash = ""
+from sys import platform
 if platform == "win32":
 	slash = "\\"
 else:
 	slash = "/"
 
-path = ""
-if "-s" in argv:
-	index = argv.index("-s") + 1
-	path = argv[index]
+if "-h" in argv:
+	print("[file path] -s (optional): where to save file, -h (optional): help, -l (required): last index that was read from. This can be 0, -c (optional): the number of indexes to be read (exclusive). If argument is not passed 100 is assumed")
 
+from os.path import exists
 if exists(argv[1]) == False:
 	raise ValueError(f"File {argv[1]} does not exists")
 
-BATCH = read_csv(argv[1], error_bad_lines=False)
-CNT = 100
+path = ""
+if "-s" in argv:
+	INDEX = argv.index("-s") + 1
+	path = f"{argv[INDEX]}{slash}"
+else:
+	from os import mkdir
+	try:
+		mkdir("Batches")
+	except FileExistsError:
+		pass
+	
+	path = f"Batches{slash}"
 
-batchFiles = array(BATCH.iloc[::CNT])
-keywordCnt = 1
-batchDF = {
-	"image_url": [],
-	"caption": []
-}
+if "-l" in argv:
+	LAST = int(argv[argv.index("-l") + 1])
+
+else:
+	raise ValueError("Expected to find -l in command line arguments")
+
+if "-c" in argv:
+	INDEX = argv.index("-c") + 1
+	CNT = int(argv[INDEX])
+else:
+	CNT = 100
+
+from pandas import read_csv
+BATCH = read_csv(argv[1], error_bad_lines=False)[LAST: CNT]
 
 cnt = 0
-for ii in batchFiles:
-	batchDF["image_url"].append(ii[0])
-	batchDF["caption"].append(ii[1])
-
-	for jj in ii[2:]:
-		batchDF[f"Keyword{keywordCnt}"] = jj
-		keywordCnt += 1
-
-	keywordCnt = 1
-	if path == "":
-		DataFrame(batchDF).to_csv(f"{cnt}.csv")
-	else:
-		DataFrame(batchDF).to_csv(f"{path}{cnt}.csv")
+while(exists(f"{path}\\batch{cnt}.csv")):
 	cnt += 1
 
-print("")
+from pandas import DataFrame
+DataFrame(BATCH).to_csv(f"{path}\\batch{cnt}.csv")
